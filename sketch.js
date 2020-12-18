512f348331091609682591ff37448d1f750e5921
AarifUzma <aarifuzma@AarifUzmas-MacBook-Air.local>
2020-12-19 01:16:01 +0530

    Platform added

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
-var platform;
+var platform,p2;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
